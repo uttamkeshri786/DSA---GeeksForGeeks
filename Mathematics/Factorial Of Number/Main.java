f3531e4ada604cf7633411f591571a9b9dbad342
Uttam keshri <uttamkeshri111114@gmail.com>
2022-01-13 02:55:33 +0530

    Create Main.java

---
+++ Mathematics/Factorial Of Number/Main.java
@@ -0,0 +1,50 @@
+// { Driver Code Starts
+// Initial Template for Java
+
+/*package whatever //do not write package name here */
+
+import java.io.*;
+import java.util.*;
+public class Main {
+    public static void main(String[] args) {
+        
+        //taking input using Scanner class
+        Scanner sc = new Scanner(System.in);
+
+        //taking testcases
+        int T = sc.nextInt();
+        while (T-- > 0) {
+            
+            //creating an object of class Factorial
+            Solution obj = new Solution();
+            int N;
+            
+            //taking N
+            N = sc.nextInt();
+            
+            //calling factorial() method 
+            //of class Factorial
+            System.out.println(obj.factorial(N));
+        }
+    }
+}
+// } Driver Code Ends
+
+
+// User function Template for Java
+
+class Solution {
+
+    public long factorial(int N) {
+        
+        if(N==0)
+        {
+           return 1;
+           
+        }
+        else
+        {
+           return N*factorial(N-1);
+        }
+    }
+}
