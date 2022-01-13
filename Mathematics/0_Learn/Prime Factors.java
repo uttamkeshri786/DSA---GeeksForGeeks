9d9d1886a0936ca4361c5cedf66d8926e3238851
Uttam keshri <uttamkeshri111114@gmail.com>
2022-01-13 12:39:29 +0530

    Update Prime Factors.java

--- Mathematics/0_Learn/Prime Factors.java
+++ Mathematics/0_Learn/Prime Factors.java
@@ -106,13 +106,21 @@ public class Main
     
 	static boolean isPrime(int n)
 	{
-	    if(n == 1)
-	        return false;
-		for(int i=2; i<n; i++)
+		if(n==1)
+			return false;
+
+		if(n==2 || n==3)
+			return true;
+
+		if(n%2==0 || n%3==0)
+			return false;
+
+		for(int i=5; i*i<=n; i=i+6)
 		{
-		    if(n%i == 0)
-	            return false;
+			if(n % i == 0 || n % (i + 2) == 0)
+				return false; 
 		}
+
 		return true;
 	}
 
