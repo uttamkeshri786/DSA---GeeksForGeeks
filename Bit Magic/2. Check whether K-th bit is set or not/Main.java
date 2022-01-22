42d1f744ea613d8109396270333c233402818d51
Uttam keshri <uttamkeshri111114@gmail.com>
2022-01-23 04:48:47 +0530

    Update Main.java

--- Bit Magic/2. Check whether K-th bit is set or not/Main.java
+++ Bit Magic/2. Check whether K-th bit is set or not/Main.java
@@ -1,3 +1,44 @@
+import java.io.*;
+import java.util.*;
+class CheckBit
+{
+    // Function to check if Kth bit is set or not.
+    public static boolean checkKthBit(int n, int k)
+    {
+        return((n & (1 << (k - 1))) > 0);
+    }  
+}
+
+public class Main
+{
+	static int n;
+	static int k;
+	
+	public static void main(String[] args) throws NumberFormatException, IOException 
+	{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		int t = Integer.parseInt(br.readLine()); //Inputting the testcases
+		
+		while(t>0) //While testcases exist
+		{
+			n = Integer.parseInt(br.readLine()); //Input N
+			k = Integer.parseInt(br.readLine()); // Input K
+			
+			CheckBit obj = new CheckBit();
+			
+			if(obj.checkKthBit(n, k))
+				System.out.println("Yes"); //If true, print Yes
+				
+			else
+				System.out.println("No");	// Else print No
+            
+            t--;
+		}
+	}	
+}
+
+/*
 // Java program to check if k-th bit of a given number is set or not
 
 import java.util.*;
@@ -20,3 +61,4 @@ public class Main {
         isKthBitSet(n, k);
     }
 }
+*/
